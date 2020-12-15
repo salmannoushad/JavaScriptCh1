28400a723791521050ac625ae3fa90f643e5c850
salmannoushad <salmansnoushad@gmail.com>
2020-12-15 22:38:05 +0600

    create variable

---
+++ ChapterOne.js
@@ -0,0 +1,17 @@
+console.log("hello world")
+console.log("i love bangladesh")
+console.log(45)
+console.log(45.9755)
+console.log('my favourite number : ' + 9)
+console.log(9 + 9)
+console.log('9' + 9)
+
+// class two
+// variables
+var name = "Salman Noushad"
+var age = 23
+console.log(name)
+console.log(age)
+console.log(name + ' knows Javascript')
+console.log('his is only 22 ' + age)
+console.log("i know javascript")
